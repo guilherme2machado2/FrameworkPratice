5e6caf8d9c910c9ff70dae48cddbb0b74e002645
guilherme2machado2 <bnwcrypee@gmail.com>
2025-03-18 16:32:05 -0300

    add dependecies

---
+++ src/main/java/com/devsworks/api_java_spring_boot/ApiJavaSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.devsworks.api_java_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaSpringBootApplication.class, args);
+		System.out.println("Esta executando!");
+	}
+
+}
